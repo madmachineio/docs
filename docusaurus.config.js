e74b3cc687595c47be7b678f0f0b175005ca5b2d
Andy Liu <andy@madmachine.io>
2022-02-01 21:27:12 +0800

    add search function
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -124,10 +124,10 @@ module.exports = {
       contextualSearch: true,
 
       // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-      externalUrlRegex: 'external\\.com|domain\\.com',
+      // externalUrlRegex: 'external\\.com|domain\\.com',
 
       // Optional: Algolia search parameters
-      searchParameters: {},
+      // searchParameters: {},
     },
     gtag: {
       // You can also use your "G-" Measurement ID here.
