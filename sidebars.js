658a8bda3d32ea8b2a786fc3e5e1ab09d8328880
Ines Zhou <ines@madmachine.io>
2022-02-21 19:05:59 +0800

    Add illustrations

--- sidebars.js
+++ sidebars.js
@@ -120,7 +120,7 @@ module.exports = {
         'tutorials/swiftio-circuit-playgrounds/button',
         'tutorials/swiftio-circuit-playgrounds/led-timer',
         'tutorials/swiftio-circuit-playgrounds/buzzer',
-        // 'tutorials/swiftio-circuit-playgrounds/potentiometer',
+        'tutorials/swiftio-circuit-playgrounds/potentiometer',
         // 'tutorials/swiftio-circuit-playgrounds/humiture',
         // 'tutorials/swiftio-circuit-playgrounds/accelerometer',
         // 'tutorials/swiftio-circuit-playgrounds/lcd',
