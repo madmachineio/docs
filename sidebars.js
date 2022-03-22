dc21d3725b3db11c2e9b931ec6af43b097108e35
Ines Zhou <ines@madmachine.io>
2022-03-22 19:21:30 +0800

    Modify shape and label explanations

--- sidebars.js
+++ sidebars.js
@@ -144,7 +144,7 @@ module.exports = {
             'tutorials/swiftio-circuit-playgrounds/display/introduction',
             'tutorials/swiftio-circuit-playgrounds/display/maddisplay',
             'tutorials/swiftio-circuit-playgrounds/display/shapes',
-            // 'tutorials/swiftio-circuit-playgrounds/display/text',
+            'tutorials/swiftio-circuit-playgrounds/display/text',
             // 'tutorials/swiftio-circuit-playgrounds/display/bmp',
             //'tutorials/swiftio-circuit-playgrounds/display/go-further',
           ],
